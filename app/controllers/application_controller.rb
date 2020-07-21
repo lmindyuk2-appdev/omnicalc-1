a19bddda85a42448bcc491670859cb42026e42ab
lmindyuk2 <lmindyuk@chicagobooth.edu>
2020-07-21 03:31:39 +0000

    starting payment form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,16 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_results.html.erb"})
   end
 
+  def blank_square_root_form
+    render({ :template => "calculation_templates/square_root_form.html.erb"})
+  end
+
+  def calculate_square_root
+    @num = params.fetch("number_to_take_a_root").to_f
+    @square_root = @num**(0.5)
+    render({ :template => "calculation_templates/square_root_results.html.erb"})
+  end
+
   def blank_random_form
     render({ :template => "calculation_templates/random_form.html.erb"})
   end
@@ -15,6 +25,18 @@ class ApplicationController < ActionController::Base
   def calculate_random
     @lower = params.fetch("user_min").to_f
     @upper = params.fetch("user_max").to_f
-    @rand = rand(@lower..@upper)
+    @result = rand(@lower..@upper)
+    render({ :template => "calculation_templates/random_results.html.erb"})
+  end
+
+  def blank_payment_form
+    render({ :template => "calculation_templates/payment_form.html.erb"})
+  end
+
+  def calculate_payment
+    @apr = params.fetch("apr_input").to_f
+    @num_years = params.fetch("years_input").to_f
+    @principal = params.fetch("principal_input").to_f
+    render({ :template => "calculation_templates/payment_results.html.erb"})
   end
 end
